458fbc4d745e7bc1a3dae5df94d1d31fb48d89ef
Sam Sheets <sam.sheets94@gmail.com>
2021-05-12 11:04:14 -0600

    insert 3 fruit documents

--- app.js
+++ app.js
@@ -17,9 +17,24 @@ async function run() {
     await client.connect();
     console.log('Connected successfully to server')
     const db = client.db(dbName);
+    insertDocuments(db)
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
 run().catch(console.dir);
+
+const insertDocuments = function(db, callback) {
+    //Get the doccuments collection
+    const collection = db.collection('fruits');
+    //insert some documents
+    collection.insertMany([
+        {name: "Apple", score: 8, review: "Great fruit"}, 
+        {name: "Orange", score: 6, review: "kinda sour"}, 
+        {name: "Banana", score: 9, review: "Great stuff!"}
+    ], function(err, result) {
+        console.log("inserted 3 documents into the collection");
+
+    });
+}
